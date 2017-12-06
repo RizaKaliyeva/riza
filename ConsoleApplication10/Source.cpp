a213c364e185d390ecb721b684410962d8c5cf6a
КалиеваР <@227_7>
2017-12-06 09:38:26 +0600

    Add project files.

---
+++ ConsoleApplication10/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+	switch()
+
+
+
+
+	system("pause");
+}
